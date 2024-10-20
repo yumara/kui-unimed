a403c11503255722e421f6c4c8c7c54cd613bd8e
Muhammad Dimas Trisandi <65803865+mdtcreation@users.noreply.github.com>
2024-10-20 16:29:49 +0700

    Adjust IBMA model to add file fields to fillable

--- app/Models/IBMA.php
+++ app/Models/IBMA.php
@@ -17,6 +17,12 @@ class IBMA extends Model
         'date_end',
         'sponsor',
         'status',
+        'file_passport',
+        'file_pasfoto',
+        'file_sk_sehat',
+        'file_soc',
+        'file_sofs',
+        'file_ijazah_transkrip',
     ];
 
     protected function casts(): array
@@ -25,5 +31,26 @@ class IBMA extends Model
             'date_start' => 'date:Y-m-d',
             'date_end' => 'date:Y-m-d',
         ];
+    }
+
+    public function isFileComplete() : bool {
+        $fields = [
+            'file_passport' => 'Jenis Kelamin',
+            'file_pasfoto' => 'Alamat',
+            'file_sk_sehat' => 'Nomor Telepon',
+            'file_soc' => 'Tanggal Lahir',
+            'file_sofs' => 'Tempat Lahir',
+            'file_ijazah_transkrip' => 'Kota',
+            // Tambahkan field lain di sini sesuai kebutuhan
+        ];
+
+        $fieldsNotFilled = [];
+        foreach ($fields as $field => $label) {
+            if (empty($this->$field)) {
+                $fieldsNotFilled[] = $label;
+            }
+        }
+
+        return empty($fieldsNotFilled);
     }
 }
