66294447de75a9cb54a16737c75f8f5ed12a1724
Muhammad Dimas Trisandi <65803865+mdtcreation@users.noreply.github.com>
2024-10-21 21:37:23 +0700

    Adding Upload Pasfoto Process

--- app/Http/Controllers/Web/User/ProfileController.php
+++ app/Http/Controllers/Web/User/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\Account\UserDataRequest;
+use App\Http\Requests\User\UserPasfotoRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\ValidationException;
@@ -30,6 +31,7 @@ class ProfileController extends Controller
                 "address" => $form['address'],
                 "city" => $form['city'],
                 "country" => $form['country'],
+                "zipcode" => $form['zipcode'],
                 "citizenship" => $form['citizenship'],
                 "occupation" => $form['occupation'],
                 "passport_id" => $form['passport_id'],
@@ -42,5 +44,22 @@ class ProfileController extends Controller
         } catch (ValidationException $e) {
             throw $e;
         }
+    }
+    public function upload(UserPasfotoRequest $request, string $id): RedirectResponse{//
+        try {
+            $form = $request->validated();
+            $fileName = $request->file('file')->hashName();
+            $path = $request->file('file')->store('uploads/user/' .$id);
+            $fields = [
+                "file_pasfoto" => $fileName,
+            ];
+            $ibma = UserData::where('user_id',$id)->update($fields);
+            return redirect()->route('user.profile')->with('message', 'Berhasil Upload Pasfoto.');
+            //return Response::make("Sukses", 200);
+            //return response()->json(['message' => 'File uploaded successfully!', 'path' => $path], 200);
+        } catch (ValidationException $e) {
+            throw $e;
+        }
+       // return response()->json(['message' => 'File upload failed!'], 500);
     }
 }
